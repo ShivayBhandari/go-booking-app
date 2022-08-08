c2f22878604b084d1059931993d0ea004c3077ad
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-08-08 14:55:38 +0530

    Variables & Constants

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt" //Package that includes Print
+import "fmt"
 
-func main() { //Start Point of the application
-	fmt.Println("Hello Mom")
+func main() {
+	var conferenceName = "Go Conference" //variable declaration
+	const conferenceTickets = 50 //constant decalration
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName,  "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "tickets are left.")
+	fmt.Println("Get your tickets here to attend")
+
 }
