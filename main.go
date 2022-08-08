e578750e07af8b1c4139434274b7d25539f14989
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-08-08 15:10:34 +0530

    Formatted Output - printf

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference" //variable declaration
-	const conferenceTickets = 50 //constant decalration
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName,  "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "tickets are left.")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v tickets are left.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 }
